1bf3d89265445e5724aa16f624a963bd4de57d97
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-04 14:10:03 +0100

    Cambios pequeños

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-const port = 8080;
+const EXPRESS = require('express');
+const FS = require('fs');
+const APP = EXPRESS();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.json());
-app.use(express.static('public'));
+APP.use(express.json());
+APP.use(express.static('public'));
 
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+APP.listen(PORT, () => {
+    console.log(`Servidor runeado en http://localhost:${PORT}`);
 });
