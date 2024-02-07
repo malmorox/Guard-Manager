1fcb9953d6993d9e031981a1fb22878dceecf967
Marcos Almorox <marcos.almorox@gmail.com>
2024-02-07 18:24:06 +0100

    Commit antes del recreo

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+const loginButton = document.getElementById("login-button")
+loginButton.addEventListener("click", function () {
+    let username = document.getElementById("user-field").value;
+    let password = document.getElementById("password-field").value;
+
+    fetch("users.json")
+        .then(response => response.json())
+        .then(users => {
+            var found = users.some(user => user.username === username && user.password === password);
+            if (found) {
+                document.querySelector(".login-popup").style.display = "none";
+            } else {
+                alert("Nombre de usuario o contraseña incorrectos");
+            }
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos de los usuarios:", error);
+        });
+});
